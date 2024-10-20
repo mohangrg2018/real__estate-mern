fba257890b261dd0f35d1eeaa5734cf9e09c4797
Mohan Gurung <mohangrg2030@gmail.com>
2024-10-20 12:04:54 +0545

    navbar logo link added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import { Search } from "lucide-react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="bg-slate-100">
       <div className="flex items-center justify-between max-w-6xl mx-auto h-[60px] px-3">
-        <h1 className="md:text-3xl font-extrabold">
-          <span className="text-blue-500">Real</span>
-          <span className="text-blue-800">State</span>
-        </h1>
+        <Link to="/">
+          <h1 className="md:text-3xl font-extrabold">
+            <span className="text-blue-500">Real</span>
+            <span className="text-blue-800">State</span>
+          </h1>
+        </Link>
         <form className="flex items-center bg-slate-200 py-2 px-4 rounded-lg">
           <input
             type="text"
@@ -18,7 +20,7 @@ const Navbar = () => {
           <Search />
         </form>
         <nav>
-          <ul className="flex items-center gap-4 sm:gap-10 md:gap-14">
+          <ul className="active__navlink flex items-center gap-4 sm:gap-10 md:gap-14">
             <li className="hidden md:block">
               <NavLink to="/" exact>
                 Home
